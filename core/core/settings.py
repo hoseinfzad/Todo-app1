91d6c9264b31792dc0f9256e222ecd84c530027c
hoseinfzad <hosein.fereydonizad@gmail.com>
2024-01-28 17:29:40 +0330

    adding rest api
    we add rest api to our project with DRF and using viewsets for this project

--- core/core/settings.py
+++ core/core/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'todo',
+    'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
--- core/todo/urls.py
+++ core/todo/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views 
 
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path("update/<str:pk>/", views.update_task, name="update_task"),
     path("complete/<str:pk>/", views.complete_task, name="complete_task"),
     path("delete/<str:pk>/", views.delete_task, name="delete_task"),
+    path('api/v1/', include('todo.api.v1.urls')),
 ]
